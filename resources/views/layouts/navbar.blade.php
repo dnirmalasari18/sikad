27a869c482d6a51067340299b6737037cd5ea114
Dewi Ayu Nirmalasari <dnirmalasari18@gmail.com>
2018-12-01 17:37:39 +0700

    template navbar fix

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -2,141 +2,33 @@
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
             <div class="navbar-header">
-                <button type="button" class="navbar-toggle" data-toggle="collapse">
+                
+                <!--<button type="button" class="navbar-toggle" data-toggle="collapse">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
-                </button>
-                <a class="navbar-brand" href="presentation.html">SIKAD</a>
+                </button>-->
+                <a class="navbar-brand" href="presentation.html"><img src="{!! asset('assets/img/logo-hmtc.jpg') !!}" height="32" ></a>
+                <a class="navbar-brand" href="presentation.html" >SIKAD</a>
             </div>
-    
+            
             <div class="collapse navbar-collapse">
                 <ul class="nav navbar-nav navbar-right">
-                    <li>
-                        <a href="index.html">
-                            <i class="material-icons">apps</i> Components
-                        </a>
-                    </li>
-    
-                    <li class="dropdown" id="headerNavigationItems">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <i class="material-icons">view_day</i> Sections
-                            <b class="caret"></b>
-                        </a>
-                        <ul class="dropdown-menu dropdown-with-icons">
-                            <li>
-                                <a href="#headers">
-                                    <i class="material-icons">dns</i> Headers
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#features">
-                                    <i class="material-icons">build</i> Features
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#blogs">
-                                    <i class="material-icons">list</i> Blogs
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#teams">
-                                    <i class="material-icons">people</i> Teams
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#projects">
-                                    <i class="material-icons">assignment</i> Projects
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#pricing">
-                                    <i class="material-icons">monetization_on</i> Pricing
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#testimonials">
-                                    <i class="material-icons">chat</i> Testimonials
-                                </a>
-                            </li>
-                            <li>
-                                <a href="#contactus">                                        <i class="material-icons">call</i> Contacts
-                                    </a>
-                            </li>
-
-                        </ul>
-                    </li>
-    
-                    <li class="dropdown">
-                        <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-                            <i class="material-icons">view_carousel</i> Examples
-                            <b class="caret"></b>
-                        </a>
-                        <ul class="dropdown-menu dropdown-with-icons">
-                            <li>
-                                <a href="examples/about-us.html">
-                                    <i class="material-icons">account_balance</i> About Us
-                                </a>
-                            </li>
-                            <li>
-                                <a href="examples/blog-post.html">
-                                    <i class="material-icons">art_track</i> Blog Post
-                                </a>
-                            </li>
-                            <li>
-                                <a href="examples/blog-posts.html">
-                                        <i class="material-icons">view_quilt</i> Blog Posts
-                                </a>
-                            </li>
-                            <li>
-                                <a href="examples/contact-us.html">
-                                    <i class="material-icons">location_on</i> Contact Us
-                                </a>
-                            </li>
-                            <li>
-                                <a href="examples/landing-page.html">
-                                    <i class="material-icons">view_day</i> Landing Page
-                                </a>
-                            </li>
-                            <li>
-                                <a href="examples/login-page.html">
-                                    <i class="material-icons">fingerprint</i> Login Page
-                                </a>
-                            </li>
-                            <li>
-                                <a href="examples/pricing.html">
-                                    <i class="material-icons">attach_money</i> Pricing Page
-                                </a>
-                            </li>
-                            <li>
-                                <a href="examples/ecommerce.html">
-                                    <i class="material-icons">shop</i> Ecommerce Page
-                                </a>
-                            </li>
-                            <li>
-                                <a href="examples/product-page.html">
-                                    <i class="material-icons">beach_access</i> Product Page
-                                </a>
-                                </li>
-                            <li>
-                                <a href="examples/profile-page.html">
-                                    <i class="material-icons">account_circle</i> Profile Page
-                                </a>
-                            </li>
-                            <li>
-                                <a href="examples/signup-page.html">
-                                    <i class="material-icons">person_add</i> Signup Page
-                                </a>
-                            </li>
-                        </ul>
-                    </li>
-    
+                    @include('layouts.menu')
+                    @guest
                     <li class="button-container">
-                        <a href="http://www.creative-tim.com/buy/material-kit-pro?ref=presentation" target="_blank" class="btn btn-white btn-round">
-                            Buy Now
+                        <a href="http://www.creative-tim.com/buy/material-kit-pro?ref=presentation" class="btn btn-white btn-round">
+                            Masuk
                         </a>
                     </li>
+                    @else
+                    <li class="button-container">
+                        <a href="http://www.creative-tim.com/buy/material-kit-pro?ref=presentation" class="btn btn-white btn-round">
+                                Masuk
+                        </a>
+                    </li>
+                    @endguest
                 </ul>
             </div>
         </div>
