d486aff8f1e6b98a0c86b1bf65e86fcf3b005e0c
Frandita Adhitama <33081060+odeloen@users.noreply.github.com>
2018-12-01 23:24:03 +0700

    Refactoring templates

---
+++ resources/views/partials/css.blade.php
@@ -0,0 +1,7 @@
+<!--     Fonts and icons     -->
+<link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" />
+
+<!-- CSS Files -->
+<link href="{!! asset('assets/css/bootstrap.min.css') !!}" rel="stylesheet" />
+<link href="{!! asset('assets/css/material-kit.css?v=1.2.1') !!}" rel="stylesheet"/>
