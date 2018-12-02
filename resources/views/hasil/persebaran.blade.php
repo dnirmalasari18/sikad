446586fd357f5ea9c554b094ee4baa4c11973aa0
Dewi Ayu Nirmalasari <dnirmalasari18@gmail.com>
2018-12-03 06:29:20 +0700

    hasi.persebaran sudah, tapi warna grafik masih perlu diubah

--- resources/views/hasil/persebaran.blade.php
+++ resources/views/hasil/persebaran.blade.php
@@ -4,6 +4,10 @@
     Persebaran
 @endsection
 
+@section('unique-css')
+<link href="{!! asset('assets/morrisjs/morris.css') !!}" rel="stylesheet">
+@endsection
+
 @section('body')
     <div class="col-md-12" style="margin-top:100px;">
         <div class="card card-nav-tabs">
@@ -18,8 +22,62 @@
             </div>
             <div class="card-content" >
                 <div class="tab-content">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <div class="panel2 panel-default2">
+                                <div class="panel-heading" style="text-align:center;font-size:15pt;">
+                                    <b>Wawasan<b>
+                                </div>
+                                <div class="panel-body">
+                                    <div id="morris-bar-chart"></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="row">
+                            <div class="col-lg-12">
+                                <div class="panel2 panel-default2">
+                                    <div class="panel-heading"style="text-align:center;font-size:15pt;">
+                                        <b>Output<b>
+                                    </div>
+                                    <div class="panel-body">
+                                        <div id="morris-line-chart"></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                 </div>
             </div>
         </div>
     </div>
+@endsection
+
+@section('unique-js')
+    <script src="{!! asset('assets/raphael/raphael.min.js') !!}"></script>
+    <script src="{!! asset('assets/morrisjs/morris.min.js') !!}"></script>
+    <script src="{!! asset('assets/data/morris-data.js') !!}"></script>
+    <script>
+        new Morris.Bar({
+            element: 'morris-bar-chart',
+            data: [
+                { parameter: 'Tidak Paham', wawasan1: 20,wawasan2: 40,wawasan3: 60,wawasan4: 30,wawasan5: 50,wawasan6: 80 },
+                { parameter: 'Paham', wawasan1: 110,wawasan2: 90,wawasan3: 70,wawasan4: 100,wawasan5: 80,wawasan6: 50 },
+            ],
+            xkey: 'parameter',
+            ykeys: ['wawasan1', 'wawasan2', 'wawasan3', 'wawasan4', 'wawasan5', 'wawasan6'],
+            labels: ['wawasan1','wawasan2','wawasan3','wawasan4','wawasan5','wawasan6'],
+        });
+        new Morris.Line({
+            element: 'morris-line-chart',
+            data: [
+                { parameter: 'Tidak Paham', output1: 70,output2: 40,output3: 60,output4: 30,output5: 50 },
+                { parameter: 'Paham', output1: 60,output2: 90,output3: 70,output4: 100,output5: 80 },
+            ],
+            xkey: 'parameter',
+            parseTime:false,
+            ykeys: ['output1', 'output2', 'output3', 'output4', 'output5'],
+            
+            labels: ['output1','output2','output3','output4','output5'],
+        });
+    </script>
 @endsection
--- resources/views/partials/js.blade.php
+++ resources/views/partials/js.blade.php
@@ -10,41 +10,37 @@
 
 
 <!--    Plugin for Date Time Picker and Full Calendar Plugin   -->
-<script src="assets/js/moment.min.js"></script>
+<!-- <script src="{!! asset('assets/js/moment.min.js') !!}"></script>-->
 
 <!--	Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/   -->
-<script src="assets/js/nouislider.min.js" type="text/javascript"></script>
+<!-- <script src="{!! asset('assets/js/nouislider.min.js') !!}" type="text/javascript"></script>-->
 
 <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker   -->
-<script src="assets/js/bootstrap-datetimepicker.js" type="text/javascript"></script>
+<!-- <script src="{!! asset('assets/js/bootstrap-datetimepicker.js') !!}" type="text/javascript"></script>-->
 
 <!--	Plugin for Select, full documentation here: http://silviomoreto.github.io/bootstrap-select   -->
-<script src="assets/js/bootstrap-selectpicker.js" type="text/javascript"></script>
+<!-- <script src="{!! asset('assets/js/bootstrap-selectpicker.js') !!}" type="text/javascript"></script>-->
 
 <!--	Plugin for Tags, full documentation here: http://xoxco.com/projects/code/tagsinput/   -->
-<script src="assets/js/bootstrap-tagsinput.js"></script>
+<!-- <script src="{!! asset('assets/js/bootstrap-tagsinput.js') !!}"></script>-->
 
 <!--	Plugin for Fileupload, full documentation here: http://www.jasny.net/bootstrap/javascript/#fileinput   -->
-<script src="assets/js/jasny-bootstrap.min.js"></script>
-
-<!--    Plugin For Google Maps   -->
-<script  type="text/javascript" src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
+<!-- <script src="{!! asset('assets/js/jasny-bootstrap.min.js') !!}"></script>-->
 
 <!--    Control Center for Material Kit: activating the ripples, parallax effects, scripts from the example pages etc    -->
-<script src="assets/js/material-kit.js?v=1.2.1" type="text/javascript"></script>
+<script src="{!! asset('assets/js/material-kit.js?v=1.2.1') !!}" type="text/javascript"></script>
 
 <script type="text/javascript">
     $().ready(function(){
-
         materialKitDemo.initContactUs2Map();
     });
 </script>
 
-    <!-- Page-Level Demo Scripts - Tables - Use for reference -->
-    <script>
-            $(document).ready(function() {
-                $('#dataTables-example').DataTable({
-                    responsive: true
-                });
-            });
-            </script>
+<!-- Page-Level Demo Scripts - Tables - Use for reference -->
+<script>
+    $(document).ready(function() {
+        $('#dataTables-example').DataTable({
+            responsive: true
+        });
+    });
+</script>
