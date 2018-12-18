811bc23b07db0d7d149808f5a765ff6d2fd41b36
Dewi Ayu Nirmalasari <dnirmalasari18@gmail.com>
2018-12-18 23:34:07 +0700

    navbar fix

--- resources/views/partials/navbars/kdpm.blade.php
+++ resources/views/partials/navbars/kdpm.blade.php
@@ -1,39 +1,58 @@
 <li>
+    <a href="{{route('user.list')}}">
+        <i class="material-icons">account_circle</i> Akun
+    </a>
+</li>
+
+<li>
     <a href="{{route('hasil.persebaran')}}">
         <i class="material-icons">bar_chart</i> Persebaran
     </a>
 </li>
-<li>
-    <a href="{{route('maba.nilai.kegiatan.list',1)}}">
-        <i class="material-icons">assignment</i>Kelola Nilai Kegiatan
-    </a>
-</li>
-<li>
-    <a href="{{route('maba.nilai.perilaku.list',1)}}">
-        <i class="material-icons">assignment</i>Kelola Nilai Perilaku
-    </a>
-</li>
 <li class="dropdown">
     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
-        <i class="material-icons">view_carousel</i> Lain-lain
+        <i class="material-icons">view_carousel</i> Kegiatan
         <b class="caret"></b>
     </a>
     <ul class="dropdown-menu dropdown-with-icons">
-        <li>
-            <a href="{{route('user.list')}}">
-                <i class="material-icons">account_circle</i> Kelola Akun
-            </a>
-        </li>
         <li>
             <a href="{{route('kegiatan.list')}}">
                 <i class="material-icons">nature_people</i> Kelola Kegiatan
             </a>
         </li>
         <li>
+            <a href="{{route('maba.nilai.kegiatan.list',1)}}">
+                <i class="material-icons">assignment</i>Kelola Nilai Kegiatan
+            </a>
+        </li>
+    </ul>
+</li>
+
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <i class="material-icons">view_carousel</i> Perilaku
+        <b class="caret"></b>
+    </a>
+    <ul class="dropdown-menu dropdown-with-icons">
+        <li>
             <a href="{{route('perilaku.list')}}">
                 <i class="material-icons">directions_run</i> Kelola Perilaku
             </a>
         </li>
+        <li>
+            <a href="{{route('maba.nilai.perilaku.list',1)}}">
+                <i class="material-icons">assignment</i>Kelola Nilai Perilaku
+            </a>
+        </li>
+    </ul>
+</li>
+
+<li class="dropdown">
+    <a href="#" class="dropdown-toggle" data-toggle="dropdown">
+        <i class="material-icons">view_carousel</i> Kurikulum
+        <b class="caret"></b>
+    </a>
+    <ul class="dropdown-menu dropdown-with-icons">
         <li>
             <a href="{{route('wawasan.list')}}">
                 <i class="material-icons">star</i>Kelola Wawasan
@@ -44,10 +63,12 @@
                 <i class="material-icons">star_border</i>Kelola Output
             </a>
         </li>
+    </ul>
+</li>
+
         <li>
             <a href="{{route('logout')}}">
                     <i class="fa fa-sign-out fa-fw"></i>Logout
             </a>
         </li>
-    </ul>
-</li>
+
